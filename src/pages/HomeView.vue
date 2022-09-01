32891add9766c70066fdc583ebb3508b861c7801
spacex_2020 <540240126@qq.com>
2022-09-01 11:16:11 +0800

    init repo

---
+++ src/pages/HomeView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>
+    <van-button type="primary">test</van-button>
+  </main>
+</template>
+
+<style scoped>
+main {
+  width: 375px;
+}
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import Components from 'unplugin-vue-components/vite';
+import { VantResolver } from 'unplugin-vue-components/resolvers';
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueJsx(),
+    Components({
+      resolvers: [VantResolver()],
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '/@/': fileURLToPath(new URL('./src', import.meta.url)),
+      '/#/': fileURLToPath(new URL('./types', import.meta.url)),
+    }
+  }
+})
