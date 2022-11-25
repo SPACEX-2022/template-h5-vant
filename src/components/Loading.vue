8e81dcc492a8c81760d6eecb1a771e04d07616b4
spacex_2020 <540240126@qq.com>
2022-11-25 14:12:08 +0800

    feat:update template

---
+++ src/components/Loading.vue
@@ -0,0 +1,25 @@
+<template>
+  <van-popup v-model:show="modelValue" overlay-class="loading-overlay" round>
+    <div class="loading-wrapper">
+      <van-loading color="#0094ff" vertical>加载中...</van-loading>
+    </div>
+  </van-popup>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps({
+  modelValue: {
+    type: Boolean,
+    default: false
+  }
+})
+</script>
+
+<style lang="scss">
+.loading-wrapper {
+  padding: 20px;
+}
+.loading-overlay {
+  backdrop-filter: blur(20px);
+}
+</style>
