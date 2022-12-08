5074e1c6bc6c2456d784f514c79862a9817eb094
spacex_2020 <540240126@qq.com>
2022-12-08 14:01:20 +0800

    feat:update template

--- build/vite/plugin/imagemin.ts
+++ build/vite/plugin/imagemin.ts
@@ -4,31 +4,34 @@ import viteImagemin from 'vite-plugin-imagemin';
 
 export function configImageminPlugin() {
   const plugin = viteImagemin({
-    gifsicle: {
-      optimizationLevel: 7,
-      interlaced: false,
-    },
-    optipng: {
-      optimizationLevel: 7,
-    },
-    mozjpeg: {
-      quality: 20,
-    },
-    pngquant: {
-      quality: [0.8, 0.9],
-      speed: 4,
-    },
-    svgo: {
-      plugins: [
-        {
-          name: 'removeViewBox',
-        },
-        {
-          name: 'removeEmptyAttrs',
-          active: false,
-        },
-      ],
-    },
+    // gifsicle: {
+    //   optimizationLevel: 7,
+    //   interlaced: false,
+    // },
+    // optipng: {
+    //   optimizationLevel: 7,
+    // },
+    // mozjpeg: {
+    //   quality: 20,
+    // },
+    // pngquant: {
+    //   quality: [0.8, 0.9],
+    //   speed: 4,
+    // },
+    // svgo: {
+    //   plugins: [
+    //     {
+    //       name: 'removeViewBox',
+    //     },
+    //     {
+    //       name: 'removeEmptyAttrs',
+    //       active: false,
+    //     },
+    //   ],
+    // },
+    webp: {
+      quality: 100
+    }
   });
   return plugin;
 }
